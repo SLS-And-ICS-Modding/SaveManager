b499aed5b9ddbd98f618762f76b2305bd9bd0ecb
Zordon1337 <Zordon1337@users.noreply.github.com>
2024-07-05 01:14:53 +0200

    Add project files.

---
+++ SaveManager/Form1.Designer.cs
@@ -0,0 +1,128 @@
+﻿namespace SaveManager
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(326, 13);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(110, 48);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Import Save";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(326, 67);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(110, 48);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Export Save";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(326, 121);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(110, 45);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Delete Save";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Items.AddRange(new object[] {
+            " "});
+            this.listBox1.Location = new System.Drawing.Point(13, 13);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(307, 251);
+            this.listBox1.TabIndex = 3;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(326, 172);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(110, 45);
+            this.button4.TabIndex = 2;
+            this.button4.Text = "Delete Save from manager";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(326, 223);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(110, 45);
+            this.button5.TabIndex = 2;
+            this.button5.Text = "Download saves";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(448, 273);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Save Manager";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+    }
+}
+
---
+++ SaveManager/Form1.cs
@@ -0,0 +1,178 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SaveManager
+{
+    public partial class Form1 : Form
+    {
+        
+        RegistryUtility reg = new RegistryUtility();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            RefreshList();
+        }
+        Dictionary<string,string> list = new Dictionary<string,string>();
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string temppatch = Path.GetTempFileName();
+            reg.ExportRegistryKey("HKEY_CURRENT_USER\\Software\\Cheesecake Dev\\Streamer Life Simulator", temppatch);
+            string output = System.IO.File.ReadAllText(temppatch);
+            string encrypted = Convert.ToBase64String(Encoding.UTF8.GetBytes(output));
+            System.IO.File.WriteAllText($"./savegames/{new Random().Next()}.slsg", $"{DateTime.Now}\n{encrypted}");
+        }
+        private void RefreshList()
+        {
+            if (!Directory.Exists("./savegames"))
+                Directory.CreateDirectory("./savegames");
+            foreach(var file in System.IO.Directory.GetFiles("./savegames"))
+            {
+                string savename = System.IO.File.ReadAllText(file).Split('\n')[0];
+                list.Add(savename, file);
+            }
+            listBox1.Items.Clear();
+            foreach(var item in list)
+            {
+                listBox1.Items.Add(item.Key);
+            }
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var msg = MessageBox.Show("This will overwrite your current save, are you sure?", "Warning", MessageBoxButtons.YesNo, MessageBoxIcon.Warning);
+            if(msg == DialogResult.Yes)
+            {
+                
+                string temppatch = Path.GetTempPath();
+                MessageBox.Show(listBox1.SelectedItem.ToString());
+                string enc = System.IO.File.ReadAllText(list[listBox1.SelectedItem.ToString()]);
+                string dec = Encoding.UTF8.GetString(Convert.FromBase64String(enc.Split('\n')[1]));
+                reg.ClearRegistryKey("Software\\Cheesecake Dev\\Streamer Life Simulator");
+                System.IO.File.WriteAllText($"{temppatch}\\import.reg", dec);
+                reg.ImportRegistryKey($"{temppatch}\\import.reg");
+            }
+            
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            var msg = MessageBox.Show("Do you want to delete your save game?","Warning",MessageBoxButtons.YesNo,MessageBoxIcon.Warning);
+            if (msg == DialogResult.Yes)
+            {
+                var msg2 = MessageBox.Show("Your save game will be deleted forever, do you want to continue?", "Last warning", MessageBoxButtons.YesNo, MessageBoxIcon.Warning);
+                if (msg2 == DialogResult.Yes)
+                    reg.ClearRegistryKey("Software\\Cheesecake Dev\\Streamer Life Simulator");
+                else
+                    return;
+            } else
+            {
+                return;
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            var msg = MessageBox.Show("Do you want to delete your save game?", "Warning", MessageBoxButtons.YesNo, MessageBoxIcon.Warning);
+            if (msg == DialogResult.Yes)
+            {
+                var msg2 = MessageBox.Show("Your save game will be deleted forever, do you want to continue?", "Last warning", MessageBoxButtons.YesNo, MessageBoxIcon.Warning);
+                if (msg2 == DialogResult.Yes)
+                    System.IO.File.Delete(list[listBox1.SelectedItem.ToString()]);
+                else
+                    return;
+            }
+            else
+            {
+                return;
+            }
+            RefreshList();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Maybe in future?");
+        }
+    }
+    public class RegistryUtility
+    {
+        public void ClearRegistryKey(string keyPath)
+        {
+            using (RegistryKey key = Registry.CurrentUser.OpenSubKey(keyPath, true))
+            {
+                if (key == null)
+                {
+                    Console.WriteLine($"Key {keyPath} not found.");
+                    return;
+                }
+
+                foreach (var valueName in key.GetValueNames())
+                {
+                    key.DeleteValue(valueName);
+                }
+
+                foreach (var subKeyName in key.GetSubKeyNames())
+                {
+                    key.DeleteSubKeyTree(subKeyName);
+                }
+            }
+        }
+        public void ExportRegistryKey(string keyPath, string exportFilePath)
+        {
+            ProcessStartInfo startInfo = new ProcessStartInfo("regedit.exe", $"/e \"{exportFilePath}\" \"{keyPath}\"")
+            {
+                RedirectStandardOutput = true,
+                UseShellExecute = false,
+                CreateNoWindow = true
+            };
+
+            using (Process process = Process.Start(startInfo))
+            {
+                process.WaitForExit();
+                if (process.ExitCode == 0)
+                {
+                    Console.WriteLine($"Registry key {keyPath} exported to {exportFilePath}.");
+                }
+                else
+                {
+                    Console.WriteLine($"Failed to export registry key {keyPath}.");
+                }
+            }
+        }
+        public void ImportRegistryKey(string importFilePath)
+        {
+            ProcessStartInfo startInfo = new ProcessStartInfo("regedit.exe", $"/s \"{importFilePath}\"")
+            {
+                RedirectStandardOutput = true,
+                UseShellExecute = false,
+                CreateNoWindow = true
+            };
+
+            using (Process process = Process.Start(startInfo))
+            {
+                process.WaitForExit();
+                if (process.ExitCode == 0)
+                {
+                    Console.WriteLine($"Registry key imported from {importFilePath}.");
+                }
+                else
+                {
+                    Console.WriteLine($"Failed to import registry key from {importFilePath}.");
+                }
+            }
+        }
+    }
+    
+}
