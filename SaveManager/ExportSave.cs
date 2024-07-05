2adf3d81d891853ff0c3488a40207b379399a996
Zordon1337 <Zordon1337@users.noreply.github.com>
2024-07-05 19:37:59 +0200

    Implemented login for ExportSave

--- SaveManager/ExportSave.cs
+++ SaveManager/ExportSave.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +13,7 @@ namespace SaveManager
 {
     public partial class ExportSave : Form
     {
+        RegistryUtility reg = new RegistryUtility();
         public ExportSave()
         {
             InitializeComponent();
@@ -19,7 +21,11 @@ namespace SaveManager
 
         private void button1_Click(object sender, EventArgs e)
         {
-
+            string temppatch = Path.GetTempFileName();
+            reg.ExportRegistryKey("HKEY_CURRENT_USER\\Software\\Cheesecake Dev\\Streamer Life Simulator", temppatch);
+            string output = System.IO.File.ReadAllText(temppatch);
+            string encrypted = Convert.ToBase64String(Encoding.UTF8.GetBytes(output));
+            System.IO.File.WriteAllText($"./savegames/{textBox1.Text}.slsg", $"{DateTime.Now}\n{encrypted}");
         }
     }
 }
