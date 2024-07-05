49d3954bdc48311923fe4bc70429e9ffbc85ccd3
Zordon1337 <Zordon1337@users.noreply.github.com>
2024-07-05 14:24:59 +0200

    Renamed Download saves to Web saves

--- SaveManager/Form1.Designer.cs
+++ SaveManager/Form1.Designer.cs
@@ -92,7 +92,7 @@
             this.button5.Name = "button5";
             this.button5.Size = new System.Drawing.Size(110, 45);
             this.button5.TabIndex = 2;
-            this.button5.Text = "Download saves";
+            this.button5.Text = "Web saves";
             this.button5.UseVisualStyleBackColor = true;
             this.button5.Click += new System.EventHandler(this.button5_Click);
             // 
